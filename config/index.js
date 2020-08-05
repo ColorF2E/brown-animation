b9248c7ec5fb539e88080d94448311701ed34653
钱张盛 <qianzhangsheng@hztianque.com>
2020-08-05 09:54:16 +0800

    修改端口

--- config/index.js
+++ config/index.js
@@ -27,7 +27,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: process.env.PORT || 4000,
+    port: process.env.PORT || 4001,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
